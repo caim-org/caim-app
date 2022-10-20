0c40b4a694ee1acf86eecac63ae41a43c07ed5a4
Al James <al.james@gmail.com>
2022-10-20 18:43:04 +0100

    Tweaks

--- caim/settings.py
+++ caim/settings.py
@@ -163,7 +163,7 @@ if MEDIA_USE_S3:
 else:
     STATIC_URL = "/static/"
     MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "media"
+    MEDIA_ROOT = BASE_DIR / "static" / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
