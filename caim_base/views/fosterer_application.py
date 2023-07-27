88eda5171549879059907da6e107e6f2996b6e35
paul <paul.genho@gmail.com>
2023-07-27 10:57:25 -0600

    foster application tentivly complete. to test

--- caim_base/views/fosterer_application.py
+++ caim_base/views/fosterer_application.py
@@ -10,33 +10,43 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
 from django.views.decorators.http import require_http_methods
 
-from ..models.fosterer import FostererProfile, FostererApplication
+from ..models.fosterer import FostererProfile, FosterApplication
+from ..models.animals import Animal
 
 @login_required()
 @require_http_methods(["POST", "GET"])
 def application(request, stage_id):
     user = request.user
 
+    # check if person has completed fosterer profile. if not send to fill.
+    try:
+        fosterer_profile = FostererProfile.objects.get(user=user)
+    except FostererProfile.DoesNotExist:
+        return redirect('/fosterer')
+
+    if not forster_profile.is_complete:
+        return redirect('/fosterer')
+
     if request.method == "POST":
         form = form_class(request.POST, instance=fosterer_profile)
 
-        animal 
+        animal_id = request.POST.get('animal_id')
 
-
-        #if form.is_valid():
-            #fosterer_profile = form.save()
-        else:
-            messages.error(request, "Please correct form errors")
-    else:
-        # check if person has completed fosterer profile. if not send to fill.
         try:
-            fosterer_profile = FostererProfile.objects.get(user=user)
-        except FostererProfile.DoesNotExist:
-            return redirect('/fosterer')
+            animal = Animal.objects.get(pk=animal_id)
+        except Animal.DoesNotExist:
+            raise Http404("No Animal matches the given query.")
 
-        if not forster_profile.is_complete:
-            return redirect('/fosterer')
+        application = FosterApplication(
+                fosterer=foster_profile, animal=animal, status='Pending', reject_reason=None)
 
+        fosterer_application.save();
+
+        # TODO check foster application for animal does not already exist and notify
+        # TODO indicate to user success on application
+        return redirect('/fosterer')
+
+    else:
         #GET method expects animal_id in query string
         animal_id = request.GET.get('animal_id', None)
         animal = get_object_or_404(Animal, pk=animal_id)
